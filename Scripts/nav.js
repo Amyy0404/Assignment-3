7f7478af1a6498d324cfa7e84e47e78507cad784
Amyy0404 <anyvv9@gmail.com>
2024-09-29 16:58:09 +0200

    Navigation + footer

---
+++ Scripts/nav.js
@@ -0,0 +1,32 @@
+const navLinks = [
+    { text: 'HOME', url:'../index.html' },
+    { text: 'DESIGNS', url: '../Design/index.html' },
+    { text: 'THEORY', url: '../Theory/index.html' },
+    { text: 'VISUALISATIONS', url: '../Visualisation/index.html' },
+];
+
+function createNavigationBar() {
+    const navElement = document.querySelector('nav');
+    if (!navElement) return; 
+
+    const navList = document.createElement('ul');
+    const currentPage = window.location.pathname.split("/").pop();
+
+    navLinks.forEach(link => {
+        const listItem = document.createElement('li');
+        const anchor = document.createElement('a');
+        anchor.textContent = link.text;
+        anchor.href = link.url;
+
+        if (link.url.includes(currentPage)) {
+            anchor.classList.add('active');
+        }
+
+        listItem.appendChild(anchor);
+        navList.appendChild(listItem);
+    });
+
+    navElement.appendChild(navList);
+}
+
+document.addEventListener('DOMContentLoaded', createNavigationBar);
